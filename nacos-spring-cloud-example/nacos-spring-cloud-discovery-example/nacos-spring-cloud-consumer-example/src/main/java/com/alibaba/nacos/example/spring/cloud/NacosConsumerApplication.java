6ee355d102cacfaea5830dd218052797fb15f540
Shawn Ke <keepthinker@163.com>
2022-08-12 00:00:13 +0800

    fix version for loadbalancer

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConsumerApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConsumerApplication.java
@@ -3,8 +3,10 @@ package com.alibaba.nacos.example.spring.cloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosConsumerApplication {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @LoadBalanced
     @Bean
@@ -39,6 +44,10 @@ public class NacosConsumerApplication {
 
         @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
         public String echo(@PathVariable String str) {
+//            ServiceInstance serviceInstance = loadBalancerClient.choose("service-provider");
+//            String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(), str);
+//            System.out.println("request url:"+url);
+
             return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
         }
     }
